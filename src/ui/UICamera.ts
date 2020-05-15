5b397f6fd4ee5faf9c90c22f536227874c345a81
bitores <773155801@qq.com>
2020-05-15 08:02:29 +0800

    f

--- src/ui/UICamera.ts
+++ src/ui/UICamera.ts
@@ -5,13 +5,13 @@ import { Vec3 } from "../math/Vec3";
 
 export class UICamera extends Camera {
   constructor() {
-    super('UI Camera', new Vec3(0,0,1));
+    super('UI Camera', new Vec3(0,0,1.1));
     // this.translateZ(1)
     // this.translateY(1)
     // this.isRightHand = true;
     // console.log(this._modelMatrix)
-    let near = 1;
-    let far = 1000;
+    let near = .1;
+    let far = 100;
     let width = 2;
     let height = 2;
     let fov = fovy(height, near);
@@ -20,7 +20,7 @@ export class UICamera extends Camera {
     // this._projectMatrix = Mat4.perspectiveWHNF(width, height, near, far, this.isRightHand); // 透视投影
     // this._projectMatrix = Mat4.perspectiveLRTBNF(-width/2, width/2, height/2, -height/2, near, far, this.isRightHand); // 透视投影
     // this._projectMatrix = Mat4.orthographicWHNF(width, height, near, far, this.isRightHand) // 正交投影
-    this._projectMatrix = Mat4.orthographicLRTBNF(-width/2, width/2, height/2, -height/2, near, far, this.isRightHand) // 正交投影
+    // this._projectMatrix = Mat4.orthographicLRTBNF(-width/2, width/2, height/2, -height/2, near, far, this.isRightHand) // 正交投影
 
   }
 }
